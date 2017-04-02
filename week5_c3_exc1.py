97ea427845849a9b306ffac04945ce67001fc0a0
Apurva Sijaria <apurvasijaria.iitkgp@gmail.com>
2017-04-02 16:38:51 +0530

    Add files via upload

---
+++ week5_c3_exc1.py
@@ -0,0 +1,20 @@
+import urllib
+import xml.etree.ElementTree as ET
+
+url = raw_input('Enter URL ')
+if len(url) < 1 : url='http://python-data.dr-chuck.net/comments_42.xml'
+
+
+uh = urllib.urlopen(url)
+data = uh.read()
+stuff=ET.fromstring(data)
+lst=stuff.findall('comments/comment')
+
+print 'Total counts',len(lst)
+sum=0
+counts = stuff.findall('.//comment')
+for values in counts:
+    b= int(values.find('count').text)
+    sum=sum+b
+print sum
+
